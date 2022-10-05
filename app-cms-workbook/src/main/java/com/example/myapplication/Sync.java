7c69174a773cafc1744491ee87964106f2e0c0bd
Filip Baranowski <baranowskif@gmail.com>
2022-10-05 14:16:28 +0200

    sync

---
+++ app-cms-workbook/src/main/java/com/example/myapplication/Sync.java
@@ -0,0 +1,92 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import db.DeviceModel;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Sync#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Sync extends Fragment {
+    ListView listview;
+    Button Addbutton;
+    EditText GetValue;
+    ArrayList<String> ListElements = new ArrayList<String>();
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String deviceId = "deviceId";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private String mdeviceId;
+
+    public Sync() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Note.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Sync newInstance(String param1, String param2) {
+        Sync fragment = new Sync();
+        Bundle args = new Bundle();
+        args.putString(deviceId, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mdeviceId = getArguments().getString(deviceId);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sync, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+}
