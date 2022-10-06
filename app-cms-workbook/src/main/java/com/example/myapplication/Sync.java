129141c4b1eea7cd798bedcca8362d33652c9e75
Filip Baranowski <baranowskif@gmail.com>
2022-10-06 21:15:26 +0200

    files download

--- app-cms-workbook/src/main/java/com/example/myapplication/Sync.java
+++ app-cms-workbook/src/main/java/com/example/myapplication/Sync.java
@@ -1,28 +1,36 @@
 package com.example.myapplication;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
-import com.google.gson.Gson;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.myapplication.databinding.FragmentSyncBinding;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.Writer;
-import java.text.SimpleDateFormat;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-
-import db.DeviceModel;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -50,6 +58,10 @@ public class Sync extends Fragment {
         // Required empty public constructor
     }
 
+    public LayoutInflater public_inflater;
+    public ViewGroup public_container;
+    private FragmentSyncBinding binding;
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -76,17 +88,102 @@ public class Sync extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sync, container, false);
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        public_inflater = inflater;
+        public_container = container;
+        binding = FragmentSyncBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getRequest();
+//            downloadFile("3269.pdf");
+//            downloadFile("qrdata.json");
+            }
+        });
+
+    }
 
 
+    public List<JSONArray> getRequest(){
+        RequestQueue volleyQueue = Volley.newRequestQueue(this.getContext());
+        String url = "https://api.info-marine.com/api/sync/qrdata/21";
+        JSONArray devices_list = null;
+        List<JSONArray> reponse_l = new ArrayList<>();
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                (Response.Listener<JSONArray>) response -> {
+                    try {
+                        getFiles(response);
+                    } finally{}
+                },
+                (Response.ErrorListener) error -> {
+                       Log.e("MainActivity", error.toString());
+                }
+        ){
+
+            /**
+             * Passing some request headers
+             */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                headers.put("apikey", "7B5zIqmRGXmrJTFmKa99vcit");
+                return headers;
+            }
+        };
+
+
+        volleyQueue.add(jsonArrayRequest);
+
+        return reponse_l;
+    }
+
+    public void getFiles(JSONArray response){
+        JSONArray jsonArray = response;
+        downloadFile("qrdata.pdf");
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                JSONObject explrObject = jsonArray.getJSONObject(i); // you will get the json object
+                String filename = (explrObject.getString("imId")) + ".pdf";
+                System.out.println(filename);
+                downloadFile(filename);
+            } catch (Exception e){
+                System.out.println(e);
+
+            }
+            //do the stuff
+        }
+
+        Toast.makeText(getActivity(), "Download Complete",
+                Toast.LENGTH_LONG).show();
+
+    }
+
+    public void downloadFile(String filename){
+        File file = new File("/storage/emulated/0/CMSData/" + filename);
+        boolean deleted = file.delete();
+        DownloadManager downloadmanager = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse("https://api.info-marine.com/api/download/" + filename);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setTitle(filename);
+        request.setDescription("Downloading");
+        request.addRequestHeader("apikey", "7B5zIqmRGXmrJTFmKa99vcit");
+        request.setVisibleInDownloadsUi(false);
+        request.setDestinationUri(Uri.parse("file:/storage/emulated/0/CMSData/" + filename));
+        downloadmanager.enqueue(request);
     }
 
 }
